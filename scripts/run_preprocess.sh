6684513d5ef06107d6f0b63bc167dd0db0cf2fab
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-20 10:59:34 +0800

    update

--- scripts/run_preprocess.sh
+++ scripts/run_preprocess.sh
@@ -14,7 +14,7 @@ TRAIN_TEXT="--text-path ../../datasets/multiLabel_text_classification/AAPD/train
 TEST_TEXT="--text-path ../../datasets/multiLabel_text_classification/AAPD/test_texts.txt"
 
 if [ ! -f data/$1/train_texts.npy ]; then
-  python preprocess.py $TRAIN_TEXT --label-path ../../datasets/multiLabel_text_classification/AAPD/train_labels.txt --vocab-path ../../datasets/multiLabel_text_classification/AAPD/vocab.npy --w2v-model ../../datasets/word_embedding/glove.840B.300d.gensim #--emb-path data/$1/emb_init.npy
+  python preprocess.py $TRAIN_TEXT --label-path ../../datasets/multiLabel_text_classification/AAPD/train_labels.txt --vocab-path ../../datasets/multiLabel_text_classification/AAPD/vocab.npy --w2v-model ../../datasets/word_embedding/glove.840B.300d.gensim --emb-path ../../datasets/multiLabel_text_classification/AAPD/emb_init.npy
 fi
 if [ ! -f data/$1/test_texts.npy ]; then
   python preprocess.py $TEST_TEXT --label-path ../../datasets/multiLabel_text_classification/AAPD/test_labels.txt --vocab-path ../../datasets/multiLabel_text_classification/AAPD/vocab.npy
