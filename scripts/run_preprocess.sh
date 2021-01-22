346b6bae521fa1f9d7211aac9df0f3af744a3f4c
wangQB666 <wangqunbo@nlsde.buaa.edu.cn>
2021-01-22 15:52:10 +0800

    update

--- scripts/run_preprocess.sh
+++ scripts/run_preprocess.sh
@@ -10,12 +10,23 @@
 #  TEST_TEXT="--text-path ../datasets/$1/test_raw_texts.txt --tokenized-path data/$1/test_texts.txt"
 #fi
 
-TRAIN_TEXT="--text-path ../../datasets/multiLabel_text_classification/AAPD/train_texts.txt"
-TEST_TEXT="--text-path ../../datasets/multiLabel_text_classification/AAPD/test_texts.txt"
+TRAIN_TEXT="--text-path ../datasets/AAPD/train_texts.txt"
+TEST_TEXT="--text-path ../datasets/AAPD/test_texts.txt"
 
 if [ ! -f data/$1/train_texts.npy ]; then
-  python preprocess.py $TRAIN_TEXT --label-path ../../datasets/multiLabel_text_classification/AAPD/train_labels.txt --vocab-path ../../datasets/multiLabel_text_classification/AAPD/vocab.npy --w2v-model ../../datasets/word_embedding/glove.840B.300d.gensim --emb-path ../../datasets/multiLabel_text_classification/AAPD/emb_init.npy
+  python preprocess.py $TRAIN_TEXT --label-path ../datasets/AAPD/train_labels.txt --vocab-path ../datasets/AAPD/vocab.npy --w2v-model ../datasets/word_embedding/glove.840B.300d.gensim --emb-path ../datasets/AAPD/emb_init.npy
 fi
 if [ ! -f data/$1/test_texts.npy ]; then
-  python preprocess.py $TEST_TEXT --label-path ../../datasets/multiLabel_text_classification/AAPD/test_labels.txt --vocab-path ../../datasets/multiLabel_text_classification/AAPD/vocab.npy
+  python preprocess.py $TEST_TEXT --label-path ../datasets/AAPD/test_labels.txt --vocab-path ../datasets/AAPD/vocab.npy
 fi
+
+
+#TRAIN_TEXT="--text-path ../../datasets/multiLabel_text_classification/AAPD/train_texts.txt"
+#TEST_TEXT="--text-path ../../datasets/multiLabel_text_classification/AAPD/test_texts.txt"
+#
+#if [ ! -f data/$1/train_texts.npy ]; then
+#  python preprocess.py $TRAIN_TEXT --label-path ../../datasets/multiLabel_text_classification/AAPD/train_labels.txt --vocab-path ../../datasets/multiLabel_text_classification/AAPD/vocab.npy --w2v-model ../../datasets/word_embedding/glove.840B.300d.gensim --emb-path ../../datasets/multiLabel_text_classification/AAPD/emb_init.npy
+#fi
+#if [ ! -f data/$1/test_texts.npy ]; then
+#  python preprocess.py $TEST_TEXT --label-path ../../datasets/multiLabel_text_classification/AAPD/test_labels.txt --vocab-path ../../datasets/multiLabel_text_classification/AAPD/vocab.npy
+#fi
